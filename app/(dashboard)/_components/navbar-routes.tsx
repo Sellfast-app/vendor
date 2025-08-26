e71e7eed5bd49b9b423df49d764413823360d422
caleb <cakpomughe@gmail.com>
2025-08-26 06:45:37 +0100

    dark mode setup 2

--- app/(dashboard)/_components/navbar-routes.tsx
+++ app/(dashboard)/_components/navbar-routes.tsx
@@ -12,12 +12,12 @@ export const NavbarRoutes = () => {
       <Search className="absolute right-2 top-3 h-4 w-4 text-muted-foreground" />
           <input
             placeholder="Search ..."
-            className=" p-2 border border-[#F5F5F5] w-full rounded-lg"
+            className=" p-2 border border-[#F5F5F5] w-full rounded-lg dark:bg-background dark:border-0"
           />
       </div>
       <div className="flex items-center gap-3">
         <IoNotificationsOutline />
-        <Button variant={"outline"} className="rounded-full">Visit Your StoreFront <HiOutlineExternalLink /></Button>
+        <Button variant={"outline"} className="rounded-full dark:bg-background">Visit Your StoreFront <HiOutlineExternalLink /></Button>
       </div>
     </div>
   );
